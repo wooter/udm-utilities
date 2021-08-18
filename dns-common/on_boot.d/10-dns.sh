aae16373445d23631c06d3f64e02ebb0a7883c8f
wooter <wouter@shush.com>
2021-08-18 18:10:58 +0200

    Update 10-dns.sh

--- dns-common/on_boot.d/10-dns.sh
+++ dns-common/on_boot.d/10-dns.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ## configuration variables:
-VLAN=5
-IPV4_IP="192.168.1.2"
+VLAN=2
+IPV4_IP="192.168.2.2"
 # This is the IP address of the container. You may want to set it to match
 # your own network structure such as 192.168.5.3 or similar.
-IPV4_GW="192.168.1.1/24"
+IPV4_GW="192.168.2.1/24"
 # As above, this should match the gateway of the VLAN for the container
 # network as above which is usually the .1/24 range of the IPV4_IP
 
